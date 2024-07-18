4a1ab9b0a977cdeda3c24bc84dfa628e278457a2
luke3.0 <lojaintegrada9@gmail.com>
2024-07-18 16:08:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
-
+const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
-const port = process.env.PORT || 4000;
-
-app.use(express.static('public')); // Sirva os arquivos estáticos do diretório 'public'
+app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+    console.log('a user connected');
 
     socket.on('move', (data) => {
-        console.log('Move:', data);
-        socket.broadcast.emit('move', data); // Envie o movimento para todos os outros clientes
+        io.emit('move', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
 });
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+server.listen(3000, () => {
+    console.log('listening on *:4000');
 });
